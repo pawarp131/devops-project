90b399ad8fde1687390a3da0197c79ff54cbd530
pawarp131 <pawarprathmesh346@gmail.com>
2026-04-17 01:59:57 +0530

    version 2

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Version 1: Hello DevOps 🚀"
+    return f"Version 2: Deployed at {datetime.now()} 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
